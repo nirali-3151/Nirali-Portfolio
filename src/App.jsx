37bc90f5cce860a3063cf33410409f511ff3ad14
nirali-3151 <145057715+nirali-3151@users.noreply.github.com>
2025-02-09 13:14:20 +0530

    skill

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import './App.css'
 import BaseNavigator from './baseNavigator/BaseNavigator'
 import { BrowserRouter } from 'react-router-dom'
+import WOW from "wow.js";
+import "animate.css";
+
 function App() {
+
+  useEffect(() => {
+    new WOW().init();
+  }, []);
 
   return (
     <>
--- src/layouts/Home/Skills/Skills.jsx
+++ src/layouts/Home/Skills/Skills.jsx
@@ -5,22 +5,30 @@ import Image from '@componants/ImageComp/Image'
 
 const Skills = () => {
   return (
+    <>
+    <div className='text-center'>
+    <div className='section-title'>My Skills</div>
+    </div>
     <div className='container skills-main-wrapper'>
       {
-        SKILL_CONST?.map((skill,index) => {
+        SKILL_CONST?.map((skill, index) => {
           return (
             <div
-            key={index}
-            className='skills-main-wrapper-inner'
+              key={index}
+              className='skills-main-wrapper-inner'
             >
               <div>
-                <Image image={skill.icon} width='100px' height='100px' />
+                <Image image={skill.icon} width='50px' height='50px' />
+              </div>
+              <div className='text1 text-center'>
+                {skill.name}
               </div>
             </div>
           )
         })
       }
     </div>
+    </>
   )
 }
 
